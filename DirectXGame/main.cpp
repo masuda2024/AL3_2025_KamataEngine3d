dc7f5ea9caaf21cda3c91b79cd60c254ed2b73a5
masuda2024 <k024g0042@g.neec.ac.jp>
2025-04-14 15:53:19 +0900

    Start

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,59 @@
+#include <Windows.h>
+#include"KamataEngine.h"
+#include"GameScene.h"
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) 
+{
+	
+	//ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	//ゲームシーンの初期化
+	gameScene->Initialize();
+
+
+
+
+	KamataEngine::Initialize(L"LE2D_21_マスダ_アキヒロ_AL3");
+	
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	while (true)
+	{
+		//エンジンの更新
+		if (KamataEngine::Update())
+		{
+			break;
+		}
+
+
+		//ゲームシーンの更新
+		gameScene->Update();
+
+		//描画開始
+		dxCommon->PreDraw();
+
+		//ゲームシーンの描画
+		gameScene->Draw();
+
+		//描画終了
+		dxCommon->PostDraw();
+
+	}
+
+	//ゲームシーンの解放
+	delete gameScene;
+	//nullptrの代入
+	gameScene = nullptr;
+
+
+
+
+	KamataEngine::Finalize();
+
+
+	return 0;
+}
